461c2f3186dd4a36247fc4647b46ca8f3f883d39
Nicholas Hydock <nhydock@gmail.com>
2014-01-26 04:03:42 -0500

    Test out input on the scene with the command parser class

--- src/Core/CommandParser.py
+++ src/Core/CommandParser.py
@@ -1,10 +1,6 @@
-
-from Command import global_commands
-import string
-
 #universal parser for the game
 #handles executing user input commands
-class CommandParser:
+class CommandParser(object):
 	def __init__(self, engine):
 		
 		#engine is required to detect and effect globally stored things
@@ -14,11 +10,11 @@ class CommandParser:
 		
 	#gets the currently typed command as a string
 	def getCurrentTypedMessage(self):
-		return string.join(self.typed, "")
+		return "".join(self.typed)
 		
 	#adds a letter to the currently typed command
 	def addLetter(self, char):
-		self.typed.append(char)
+		self.typed.append(str(char))
 	
 	#pop off the last letter
 	def removeLetter(self):
@@ -33,17 +29,14 @@ class CommandParser:
 		#ensure it's lower case, all commands should be registered in lower case so then case doesn't matter
 		s = self.getCurrentTypedMessage().lower() 
 		self.typed = []	#clear the command when it goes to execute
-		print s
+		print(s)
 		
 		command = None
 		#default show message that thinks the command is wrong
-		self.engine.show("What are you even trying to do?")
+		#self.engine.show("What are you even trying to do?")
 		try:
 			#check against globally known commands and against room/scenario specific commands
-			for c in global_commands+self.engine.getScenario().getCommands():
-				if str(c) in s: #look for the name of the command in the string
-					c.execute(s)
-					return
+			pass
 		except Exception as e:
-			print e
+			print(e)
 		
--- src/Scenes/Test/TestScene.py
+++ src/Scenes/Test/TestScene.py
@@ -1,4 +1,5 @@
 import sfml as sf
+from Core.CommandParser import CommandParser
 from Utils.Scene import Scene
 
 
@@ -8,14 +9,66 @@ class TestScene(Scene):
 		texture = sf.Texture.load_from_file('../data/window.png')
 		self.sprite = sf.Sprite(texture)
 
+		#load up font
+		font = sf.Font.load_from_file("../data/fonts/default.ttf")
+		self.text = sf.Text();
+		self.text.font = font
+		self.text.color = sf.Color.BLACK
+		self.text.character_size = 24
+		self.text.position = (10, 450);
+
+		self.cp = CommandParser(None)
+		self.move = [False, False, False, False]
+
 		return True
 
+	def key_pressed(self, event):
+		#type input
+		if event.type == sf.Event.TEXT_ENTERED:
+			if (event.unicode == '\b'):
+				self.cp.removeLetter()
+			else:
+				self.cp.addLetter(event.unicode);
+
+		#move the test sprite around
+		elif event.code == sf.Keyboard.LEFT:
+			self.move[0] = True
+		elif event.code == sf.Keyboard.RIGHT:
+			self.move[1] = True
+		elif event.code == sf.Keyboard.UP:
+			self.move[2] = True
+		elif event.code == sf.Keyboard.DOWN:
+			self.move[3] = True
+		
+	def key_released(self, event):
+		if event.code == sf.Keyboard.LEFT:
+			self.move[0] = False
+		elif event.code == sf.Keyboard.RIGHT:
+			self.move[1] = False
+		elif event.code == sf.Keyboard.UP:
+			self.move[2] = False
+		elif event.code == sf.Keyboard.DOWN:
+			self.move[3] = False
+		
+
 	def update(self, delta):
-		#handle key input
-		if sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT):
-			self.sprite.move(50.0*delta, 0)
-		elif sf.Keyboard.is_key_pressed(sf.Keyboard.LEFT):
-			self.sprite.move(-50.0*delta, 0)
+		#handle horizontal movement
+		xrate = 0
+		if self.move[0]:
+			xrate -= 50
+		elif self.move[1]:
+			xrate += 50
+		
+		#handle vertical movement
+		yrate = 0
+		if self.move[2]:
+			yrate -= 50
+		elif self.move[3]:
+			yrate += 50
+
+		self.sprite.move(xrate * delta, yrate * delta);
+		self.text.string = self.cp.getCurrentTypedMessage()
 
 	def render(self, context, delta):
 		context.draw(self.sprite)
+		context.draw(self.text)
